efc1071d7ed793527e2118bba404869587e5e5e2
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-12-25 19:02:03 +0300

    lora initalisation part 1 & lora board modes

---
+++ lora-sx127x/lora.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include "lora.h"
+
+lora_config_t default_config = {
+    .m0 = 4,
+    .m1 = 5,
+    .tx = 1,
+    .rx = 2,
+    .baudrate = BAUDRATE_9600,
+    .uart_id = 0,
+};
+
+void lora_init(lora_config_t *config)
+{
+    if (config == NULL)
+    {
+        config = &default_config;
+    }
+
+    gpio_init(config->m0);
+    gpio_init(config->m1);
+    gpio_init(config->tx);
+    gpio_init(config->rx);
+
+    gpio_set_dir(config->m0, GPIO_OUT);
+    gpio_set_dir(config->m1, GPIO_OUT);
+    gpio_set_dir(config->tx, GPIO_OUT);
+    gpio_set_dir(config->rx, GPIO_IN);
+
+    gpio_put(config->m0, 0);
+    gpio_put(config->m1, 0);
+
+    uart_init(config->uart_id > 0 ? uart1 : uart0, config->baudrate);
+    gpio_set_function(config->tx, GPIO_FUNC_UART);
+    gpio_set_function(config->rx, GPIO_FUNC_UART);
+
+    lora_reset(config);
+}
+
+void lora_reset(lora_config_t *config)
+{
+    lora_sleep_mode(config); // set sleep mode to reset module
+
+    char command_hex = 0xC4;
+    char command[3] = {command_hex, command_hex, command_hex};
+
+    uart_puts(config->uart_id > 0 ? uart1 : uart0, command); //reset module
+
+    sleep_ms(2000);
+
+    lora_normal_mode(config); // set normal mode
+}
+
+void lora_sleep_mode(lora_config_t *config)
+{
+    if (config == NULL)
+    {
+        config = &default_config;
+    }
+
+    gpio_put(config->m0, 1);
+    gpio_put(config->m1, 1);
+}
+
+void lora_normal_mode(lora_config_t *config)
+{
+    if (config == NULL)
+    {
+        config = &default_config;
+    }
+
+    gpio_put(config->m0, 0);
+    gpio_put(config->m1, 0);
+}
+
+void lora_powersave_mode(lora_config_t *config)
+{
+    if (config == NULL)
+    {
+        config = &default_config;
+    }
+
+    gpio_put(config->m0, 1);
+    gpio_put(config->m1, 0);
+}
---
+++ lora-sx127x/lora.h
@@ -0,0 +1,47 @@
+typedef enum lora_transaction_speed {
+    VERY_SLOW,
+    SLOW,
+    MEDIUM,
+    FAST,
+    VERY_FAST,
+    ULTRA_FAST
+} lora_transaction_speed_t;
+
+typedef enum lora_baudrate {
+    BAUDRATE_1200=1200,
+    BAUDRATE_2400=2400,
+    BAUDRATE_4800=4800,
+    BAUDRATE_9600=9600,
+    BAUDRATE_19200=19200,
+    BAUDRATE_38400=38400,
+    BAUDRATE_57600=57600,
+    BAUDRATE_115200=115200
+} lora_baudrate_t;
+
+typedef struct lora_config  {
+    uint8_t m0;
+    uint8_t m1;
+    uint8_t tx;
+    uint8_t rx;
+    uint8_t uart_id;
+    lora_baudrate_t baudrate;
+    lora_transaction_speed_t speed;
+} lora_config_t;
+
+void lora_init(lora_config_t *config);
+
+void lora_reset(lora_config_t *config);
+
+void lora_configure(lora_config_t *config);
+
+void lora_send_raw(uint8_t *data);
+
+void lora_send(struct stream data);
+
+void lora_receive(struct stream *data, uint8_t len);
+
+void lora_sleep_mode(lora_config_t *config);
+
+void lora_normal_mode(lora_config_t *config);
+
+void lora_powersave_mode(lora_config_t *config);
